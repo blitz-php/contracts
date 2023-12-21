ea47e0d473365ba7612950aadd82fd502ac11c33
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2023-12-21 18:30:18 +0100

    feat : Ajout des interfaces Autoloader, Filesystem, Renderer et Mailer
    Ce commit ajoute trois nouveaux fichiers d'interface : `LocatorInterface`, `FilesystemInterface`, `RendererInterface` et `MailerInterface`.
    Ces interfaces définissent les contrats pour le chargement automatique des fichiers, l'interaction avec le système de fichiers, le rendu de la vue et l'envoi de courriels, respectivement.
    L'ajout de ces interfaces pose les bases de l'implémentation de ces fonctionnalités dans le cadre de BlitzPHP.

---
+++ Autoloader/LocatorInterface.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework.
+ *
+ * (c) 2022 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Contracts\Autoloader;
+
+/**
+ * Permet de charger des fichiers non-classes avec un espace de noms.
+ * Fonctionne avec les helpers, les vues, etc.
+ */
+interface LocatorInterface
+{
+    /**
+     * Tente de localiser un fichier en examinant le nom d'un espace de noms
+     * et en parcourant les fichiers d'espace de noms PSR-4 que nous connaissons.
+     *
+     * @param string      $file   Le fichier d'espace de noms à localiser
+     * @param string|null $folder Le dossier dans l'espace de noms où nous devons rechercher le fichier.
+     * @param string      $ext    L'extension de fichier que le fichier doit avoir.
+     *
+     * @return false|string Le chemin d'accès au fichier, ou false s'il n'est pas trouvé.
+     */
+    public function locateFile(string $file, ?string $folder = null, string $ext = 'php');
+
+    /**
+     * Examine une fichier et retourne le FQCN.
+     */
+    public function getClassname(string $file): string;
+
+    /**
+     * Recherche dans tous les espaces de noms définis à la recherche d'un fichier.
+     * Renvoie un tableau de tous les emplacements trouvés pour le fichier défini.
+     *
+     * Exemple:
+     *
+     *  $locator->search('Config/Routes.php');
+     *  // Assuming PSR4 namespaces include foo and bar, might return:
+     *  [
+     *      'app/Modules/foo/Config/Routes.php',
+     *      'app/Modules/bar/Config/Routes.php',
+     *  ]
+     */
+    public function search(string $path, string $ext = 'php', bool $prioritizeApp = true): array;
+
+    /**
+     * Recherchez le nom qualifié d'un fichier en fonction de l'espace de noms du premier chemin d'espace de noms correspondant.
+     *
+     * @return false|string Le nom qualifié ou false si le chemin n'est pas trouvé
+     */
+    public function findQualifiedNameFromPath(string $path);
+
+    /**
+     * Scane les namespace definis, retourne une liste de tous les fichiers
+     * contenant la sous partie specifiee par $path.
+     *
+     * @return string[] Liste des fichiers du chemins
+     */
+    public function listFiles(string $path): array;
+
+    /**
+     * Analyse l'espace de noms fourni, renvoyant une liste de tous les fichiers
+     * contenus dans le sous-chemin spécifié par $path.
+     *
+     * @return string[] Liste des chemins des fichiers
+     */
+    public function listNamespaceFiles(string $prefix, string $path): array;
+}
---
+++ Filesystem/FilesystemInterface.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * This file is part of Blitz PHP framework - Filesystem.
+ *
+ * (c) 2023 Dimitri Sitchet Tomkeu <devcode.dst@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BlitzPHP\Contracts\Filesystem;
+
+/**
+ * L'interface de manipulation du systeme de fichier de BlitzPHP
+ */
+interface FilesystemInterface
+{
+    /**
+     * Visibilité publique.
+     *
+     * @var string
+     */
+    public const VISIBILITY_PUBLIC = 'public';
+
+    /**
+     * Visibilité privée.
+     *
+     * @var string
+     */
+    public const VISIBILITY_PRIVATE = 'private';
+
+    /**
+     * Déterminer si un fichier existe.
+     */
+    public function exists(string $path): bool;
+
+    /**
+     * Obtenir le contenu d'un fichier.
+     */
+    public function get(string $path): ?string;
+
+    /**
+     * Obtenir une ressource pour lire le fichier.
+     *
+     * @return resource|null The path resource or null on failure.
+     */
+    public function readStream(string $path);
+
+    /**
+     * Écrire le contenu d'un fichier.
+     *
+     * @param resource|string $contents
+     *
+     * @return bool
+     */
+    public function put(string $path, $contents, mixed $options = []);
+
+    /**
+     * Écriture d'un nouveau fichier à l'aide d'un flux.
+     *
+     * @param resource $resource
+     *
+     * @return bool
+     */
+    public function writeStream(string $path, $resource, array $options = []);
+
+    /**
+     * Obtenir la visibilité du chemin donné.
+     */
+    public function getVisibility(string $path): string;
+
+    /**
+     * Définir la visibilité du chemin donné.
+     */
+    public function setVisibility(string $path, string $visibility): bool;
+
+    /**
+     * Ajouter un contenu textuel au debut d'un fichier.
+     */
+    public function prepend(string $path, string $data): bool;
+
+    /**
+     * Ajouter un contenu textuel à la fin d'un fichier.
+     */
+    public function append(string $path, string $data): bool;
+
+    /**
+     * Supprime le fichier situé à un emplacement donné.
+     */
+    public function delete(string|array $paths): bool;
+
+    /**
+     * Copier un fichier vers un nouvel emplacement.
+     */
+    public function copy(string $from, string $to): bool;
+
+    /**
+     * Déplacer un fichier vers un nouvel emplacement.
+     */
+    public function move(string $from, string $to): bool;
+
+    /**
+     * Obtenir la taille d'un fichier donné.
+     */
+    public function size(string $path): int;
+
+    /**
+     * Obtenir l'heure de la dernière modification du fichier.
+     */
+    public function lastModified(string $path): int;
+
+    /**
+     * Obtenir un tableau de tous les fichiers d'un répertoire.
+     */
+    public function files(?string $directory = null, bool $recursive = false): array;
+
+    /**
+     * Récupère tous les fichiers du répertoire donné (récursif).
+     */
+    public function allFiles(?string $directory = null): array;
+
+    /**
+     * Obtenir tous les répertoires d'un répertoire donné.
+     */
+    public function directories(?string $directory = null, bool $recursive = false): array;
+
+    /**
+     * Obtenir tous les répertoires (récursifs) d'un répertoire donné.
+     */
+    public function allDirectories(?string $directory = null): array;
+
+    /**
+     * Créer un répertoire.
+     */
+    public function makeDirectory(string $path): bool;
+
+    /**
+     * Supprimer un répertoire de manière récursive.
+     */
+    public function deleteDirectory(string $directory): bool;
+}
