3878c95c2206d4b978b0dd882b345fdd18022c10
Dimitri Sitchet Tomkeu <dimitrisitchet@gmail.com>
2024-03-24 12:19:22 +0100

    feat: ajout des code de statut valides

--- Http/StatusCode.php
+++ Http/StatusCode.php
@@ -23,8 +23,7 @@ interface StatusCode
     /**
      * Codes non officiels
      */
-    public const CHECKPOINT = 103;
-
+    public const CHECKPOINT                           = 103;
     public const THIS_IS_FIND                         = 218;  // Apache Web Server
     public const PAGE_EXPIRED                         = 419;  // Laravel Framework
     public const METHOD_FAILURE                       = 420;  // Spring Framework
@@ -52,7 +51,9 @@ interface StatusCode
     public const RAILGUN_ERROR                        = 527;  // Cloudflare
     public const SITE_IS_OVERLOADED                   = 529;  // Qualys in the SSLLabs
     public const SITE_IS_FROZEN                       = 530;  // Pantheon web platform
+    public const TEMPORARILY_DISABLED                 = 540;  // Pantheon web platform
     public const NETWORK_READ_TIMEOUT_ERROR           = 598;  // Informal convention
+    public const NETWORK_CONNECT_TIMEOUT_ERROR        = 599;  // Informal convention
 
     /**
      * Code officiels
@@ -128,5 +129,90 @@ interface StatusCode
     public const INSUFFICIENT_STORAGE            = 507;  // WebDAV; RFC 4918
     public const LOOP_DETECTED                   = 508;  // WebDAV; RFC 4918
     public const NOT_EXTENDED                    = 510;  // RFC 2774
-    public const NOTWORK_AUTHENTICATION_REQUIRED = 511;  // RFC 6585
+    public const NETWORK_AUTHENTICATION_REQUIRED = 511;  // RFC 6585
+
+	/**
+     * Codes d'état HTTP autorisés et leur description par défaut.
+     *
+     * @var array<int, string>
+     */
+	public const VALID_CODES = [
+        // 1xx: Informational
+        self::CONTINUE            => 'Continue',
+        self::SWITCHING_PROTOCOLS => 'Switching Protocols',
+        self::PROCESSING          => 'Processing',            // http://www.iana.org/go/rfc2518
+        self::EARLY_HINTS         => 'Early Hints',           // http://www.ietf.org/rfc/rfc8297.txt
+        // 2xx: Success
+        self::OK                            => 'OK',
+        self::CREATED                       => 'Created',
+        self::ACCEPTED                      => 'Accepted',
+        self::NON_AUTHORITATIVE_INFORMATION => 'Non-Authoritative Information',   // 1.1
+        self::NO_CONTENT                    => 'No Content',
+        self::RESET_CONTENT                 => 'Reset Content',
+        self::PARTIAL_CONTENT               => 'Partial Content',
+        self::MULTI_STATUS                  => 'Multi-Status',                    // http://www.iana.org/go/rfc4918
+        self::ALREADY_REPORTED              => 'Already Reported',                // http://www.iana.org/go/rfc5842
+        self::IM_USED                       => 'IM Used',                         // 1.1; http://www.ietf.org/rfc/rfc3229.txt
+        // 3xx: Redirection
+        self::MULTIPLE_CHOICES   => 'Multiple Choices',
+        self::MOVED_PERMANENTLY  => 'Moved Permanently',
+        self::FOUND              => 'Found',                // Formerly 'Moved Temporarily'
+        self::SEE_OTHER          => 'See Other',            // 1.1
+        self::NOT_MODIFIED       => 'Not Modified',
+        self::USE_PROXY          => 'Use Proxy',            // 1.1
+        self::SWITCH_PROXY       => 'Switch Proxy',         // No longer used
+        self::TEMPORARY_REDIRECT => 'Temporary Redirect',   // 1.1
+        self::PERMANENT_REDIRECT => 'Permanent Redirect',   // 1.1; Experimental; http://www.ietf.org/rfc/rfc7238.txt
+        // 4xx: Client error
+        self::BAD_REQUEST                     => 'Bad Request',
+        self::UNAUTHORIZED                    => 'Unauthorized',
+        self::PAYMENT_REQUIRED                => 'Payment Required',
+        self::FORBIDDEN                       => 'Forbidden',
+        self::NOT_FOUND                       => 'Not Found',
+        self::METHOD_NOT_ALLOWED              => 'Method Not Allowed',
+        self::NOT_ACCEPTABLE                  => 'Not Acceptable',
+        self::PROXY_AUTHENTICATION_REQUIRED   => 'Proxy Authentication Required',
+        self::REQUEST_TIMEOUT                 => 'Request Timeout',
+        self::CONFLICT                        => 'Conflict',
+        self::GONE                            => 'Gone',
+        self::LENGTH_REQUIRED                 => 'Length Required',
+        self::PRECONDITION_FAILED             => 'Precondition Failed',
+        self::PAYLOAD_TOO_LARGE               => 'Content Too Large',                 // https://www.iana.org/assignments/http-status-codes/http-status-codes.xml
+        self::URI_TOO_LONG                    => 'URI Too Long',                      // https://www.iana.org/assignments/http-status-codes/http-status-codes.xml
+        self::UNSUPPORTED_MEDIA_TYPE          => 'Unsupported Media Type',
+        self::RANGE_NOT_SATISFIABLE           => 'Requested Range Not Satisfiable',
+        self::EXPECTATION_FAILED              => 'Expectation Failed',
+        self::IM_A_TEAPOT                     => "I'm a teapot",                      // April's Fools joke; http://www.ietf.org/rfc/rfc2324.txt
+        self::MISDIRECTED_REQUEST             => 'Misdirected Request',               // http://www.iana.org/go/rfc7540 Section 9.1.2
+        self::UNPROCESSABLE_ENTITY            => 'Unprocessable Content',             // https://www.iana.org/assignments/http-status-codes/http-status-codes.xml
+        self::LOCKED                          => 'Locked',                            // http://www.iana.org/go/rfc4918
+        self::FAILED_DEPENDENCY               => 'Failed Dependency',                 // http://www.iana.org/go/rfc4918
+        self::TOO_EARLY                       => 'Too Early',                         // https://datatracker.ietf.org/doc/draft-ietf-httpbis-replay/
+        self::UPGRADE_REQUIRED                => 'Upgrade Required',
+        self::PRECONDITION_REQUIRED           => 'Precondition Required',             // 1.1; http://www.ietf.org/rfc/rfc6585.txt
+        self::TOO_MANY_REQUESTS               => 'Too Many Requests',                 // 1.1; http://www.ietf.org/rfc/rfc6585.txt
+        self::REQUEST_HEADER_FIELDS_TOO_LARGE => 'Request Header Fields Too Large',   // 1.1; http://www.ietf.org/rfc/rfc6585.txt
+        self::UNAVAILABLE_FOR_LEGAL_REASONS   => 'Unavailable For Legal Reasons',     // http://tools.ietf.org/html/rfc7725
+        // 5xx: Server error
+        self::INTERNAL_ERROR                  => 'Internal Server Error',
+        self::NOT_IMPLEMENTED                 => 'Not Implemented',
+        self::BAD_GATEWAY                     => 'Bad Gateway',
+        self::SERVICE_UNAVAILABLE             => 'Service Unavailable',
+        self::GATEWAY_TIMEOUT                 => 'Gateway Timeout',
+        self::HTTP_VERSION_NOT_SUPPORTED      => 'HTTP Version Not Supported',
+        self::VARIANT_ALSO_NEGOTIATES         => 'Variant Also Negotiates',           // 1.1; http://www.ietf.org/rfc/rfc2295.txt
+        self::INSUFFICIENT_STORAGE            => 'Insufficient Storage',              // http://www.iana.org/go/rfc4918
+        self::LOOP_DETECTED                   => 'Loop Detected',                     // http://www.iana.org/go/rfc5842
+        self::NOT_EXTENDED                    => 'Not Extended',                      // http://www.ietf.org/rfc/rfc2774.txt
+        self::NETWORK_AUTHENTICATION_REQUIRED => 'Network Authentication Required',   // http://www.ietf.org/rfc/rfc6585.txt
+        // Not officially supported
+		self::PAGE_EXPIRED                  => 'Page Expired',
+		self::INVALID_TOKEN                 => 'Invalid Token',
+		self::CLIENT_CLOSED_REQUEST         => 'Client Closed Request',                // http://lxr.nginx.org/source/src/http/ngx_http_request.h#0133
+		self::NO_RESPONSE                   => 'Connection Closed Without Respon0se',
+		self::SITE_IS_OVERLOADED            => 'Site Is Overloaded',
+		self::SITE_IS_FROZEN                => 'Site Is Frozen',
+		self::TEMPORARILY_DISABLED          => 'Temporarily Disabled',
+		self::NETWORK_CONNECT_TIMEOUT_ERROR => 'Network Connect Timeout Error',        // https://httpstatuses.com/599
+    ];
 }
